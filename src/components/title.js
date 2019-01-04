fabee4cdb2b0c823e05f75d3ba4f387559b11000
Selçuk İTMİŞ <slck87@hotmail.com>
2019-01-04 15:44:05 +0300

    Initial commit

---
+++ src/components/title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class title extends Component {
+    render() {
+        return (
+            <h4>{this.props.text}</h4>
+        );
+    }
+}
+
+export default title;
